43eb6692a53c6396345072aaa519fc3dc0886b3b
Tilly-py <alex.s.tilly@outlook.com>
2025-12-13 12:07:12 +0100

    Upp med kort

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        playfair: ["'Playfair Display'", "serif"],
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        "roboto-slab": ["'Roboto Slab'", "serif"],
+      },
+    },
+  },
+};
