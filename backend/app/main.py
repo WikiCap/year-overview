897f8fc1f3b49a2a3618034b74f7d92531d5f13b
Tilly-py <alex.s.tilly@outlook.com>
2026-01-02 17:14:01 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,6 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from backend.resources.wiki_service import fetch_year_events
 from backend.resources.wiki_nobel import get_nobel_prizes
+from backend.resources.wiki_year_recap import get_year_recap
 
 app = FastAPI()
 
@@ -39,3 +40,10 @@ def get_year(year: int):
 @app.get("/api/year/{year}/nobel")
 def year_nobel(year: int):
     return get_nobel_prizes(year)
+
+@app.get("/api/year/{year}/recap")
+def year_recap(year: int):
+    return {
+        "year": year,
+        "recap": get_year_recap(year)
+    }
